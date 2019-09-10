05860a04d6de3e871b0001d0434b28b7c4315ec8
loicprodhom <54361074+loicprodhom@users.noreply.github.com>
2019-09-10 13:13:41 +0300

    First Commit

---
+++ src/main/java/com/project/bookstore/domain/Book.java
@@ -0,0 +1,8 @@
+package com.project.bookstore.domain;
+
+public class Book {
+	// title, author, year, isbn, price 
+	private String title, author, isbn;
+	private int year;
+	private double price;
+}
