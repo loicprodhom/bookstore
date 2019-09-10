73ee7e725a6b3e054ebde2a644147eae782f79c7
loicprodhom <54361074+loicprodhom@users.noreply.github.com>
2019-09-10 13:44:03 +0300

    Updating Project

--- src/main/java/com/project/bookstore/BookstoreApplication.java
+++ src/main/java/com/project/bookstore/BookstoreApplication.java
@@ -1,13 +1,27 @@
 package com.project.bookstore;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.project.bookstore.domain.*;
+
+import com.project.bookstore.domain.BookRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner demo(BookRepository repository) {
+		return (args) -> {
+			repository.save(new Book("1984", "George Orwell","XXXXX",1984,9.99));
+			repository.save(new Book("Le Signal", "Maxime Chattam","YYYYY",2018,23.99));
+		};
 	}
 
 }
--- src/main/java/com/project/bookstore/domain/Book.java
+++ src/main/java/com/project/bookstore/domain/Book.java
@@ -1,8 +1,26 @@
 package com.project.bookstore.domain;
+import javax.persistence.*;
 
+@Entity
 public class Book {
 	// title, author, year, isbn, price 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
 	private String title, author, isbn;
 	private int year;
 	private double price;
+	
+	public Book(String title, String author, String isbn, int year, double price) {
+		this.title=title;
+		this.author=author;
+		this.isbn=isbn;
+		this.year=year;
+		this.price=price;
+	}
+	
+	@Override
+	public String toString() {
+		return "Book id="+id+", title="+title+", author="+author+", isbn="+isbn+", year="+year+", price="+price;
+	}
 }
